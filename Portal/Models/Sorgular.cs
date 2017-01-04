4a7eab119a1a0d868cfb0a976009b519734a479e
ozcanbaygus <ozcanbaygus@gmail.com>
2017-01-04 13:19:59 +0300

    SQL sorgusu güncellendi.

--- Portal/Models/Sorgular.cs
+++ Portal/Models/Sorgular.cs
@@ -7,47 +7,14 @@ namespace Portal.Models
 {
     public static class Sorgular
     {
-        public static IEnumerable<isler> GetirIsler(this IEnumerable<isler> kaynakTablo, bool kontrolBekleyenIsler, bool onaylananIsler, string kullaniciID, int? RefBolgeID)
+        public static IEnumerable<isler> GetirIsler(this IEnumerable<isler> kaynakTablo, bool? kontrolBekleyenIsler, bool? onaylananIsler, string kullaniciID, int? RefBolgeID)
         {
-            //kontrolbekleyen işler
-            if (kontrolBekleyenIsler == true && onaylananIsler == false)
-            {
-                return (from d in kaynakTablo
-                        where d.islerinisinOnayDurumu != true && d.islerisinTamamlanmaDurumu == true && d.islerisiVerenKisi == kullaniciID
-                        orderby d.islerTarih descending
-                        select d).ToList();
-            }
-            //yeni işler
-            else if (onaylananIsler == false && kontrolBekleyenIsler == false)
-            {
-                List<int> idler;
 
-                using (var db = new PortalEntities())
-                {
-                    idler = db.IsiYapacakKisis.Where(a => a.RefIsiYapacakKisiUserID == kullaniciID).Select(x => x.RefIsID).ToList();
-                }
-
-
-                return from d in kaynakTablo
-                       where d.islerinisinOnayDurumu != true && d.islerisinTamamlanmaDurumu == false && idler.Contains(d.islerID)
-                       orderby d.islerTarih descending
-                       select d;
-            }
-
-            else if (onaylananIsler == true && kontrolBekleyenIsler == true)
-            {
-                return from d in kaynakTablo
-                       where d.islerinisinOnayDurumu == true && d.islerisinTamamlanmaDurumu == true && d.islerisiVerenKisi == kullaniciID
-                       orderby d.islerTarih descending
-                       select d;
-            }
-            else
-            {
-                return from d in kaynakTablo
-                       where d.islerisiVerenKisi == kullaniciID
-                       orderby d.islerTarih descending
-                       select d;
-            }
+            return (from d in kaynakTablo
+                    where (kontrolBekleyenIsler == true ? d.islerinisinOnayDurumu == true : d.islerinisinOnayDurumu == false)
+                    &&    (onaylananIsler == true ? d.islerinisinOnayDurumu == true : d.islerinisinOnayDurumu == false)
+                    &&    (kullaniciID == null ? true : d.islerisiVerenKisi == kullaniciID)
+                    select d).ToList();
 
 
         }
