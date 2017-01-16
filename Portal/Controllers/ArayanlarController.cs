6c77270b82d73066cc3106de39023d7c6d539b3b
fatih <fatihgokce07@gmail.com>
2017-01-16 14:59:07 +0300

    -arayan ekle tasarimi yapiliyor

---
+++ Portal/Controllers/ArayanlarController.cs
@@ -0,0 +1,23 @@
+﻿using Portal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portal.Controllers
+{
+    public class ArayanlarController : BaseController
+    {
+        // GET: Arayanlar
+        public ActionResult ArayanEkle()
+        {
+            Arayanlar arayanlar = new Arayanlar();
+            isler isler = new isler();
+            ViewBag.arayanGrupID = new SelectList(Db.ArayanGrups, "arayanGrupID", "arayanGrupAdi");
+            ViewBag.arayanRefKonumID = new SelectList(Db.Konums, "KonumID", "Konum1");
+            ViewBag.arayanDomainKategoriID = new SelectList(Db.DomainKategoris, "DomainKategoriID", "DomainKategoriAdi");
+            return View(FrmView.Data.WithArayanlar(arayanlar).WithIsler(isler));
+        }
+    }
+}
---
+++ Portal/Models/FrmViewData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portal.Models
+{
+    public class FrmViewData
+    {
+        public Arayanlar Arayanlar { get; set; }
+        public isler Isler { get; set; }
+        public FrmViewData WithArayanlar(Arayanlar entity)
+        {
+            this.Arayanlar = entity;
+            return this;
+        }
+        public FrmViewData WithIsler(isler entity)
+        {
+            this.Isler = entity;
+            return this;
+        }
+    }
+    public class FrmView
+    {
+        public static FrmViewData Data { get { return new FrmViewData(); } }
+    }
+}
