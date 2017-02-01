ec7010f1bfa798d013f9e08f90beedfbf23a8d1b
fatih <fatihgokce07@gmail.com>
2017-02-01 14:44:14 +0300

    -anasayfaya da projeler kısmına, iş yapılan son 10 domain geliyor

--- Portal/Controllers/HomeController.cs
+++ Portal/Controllers/HomeController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Portal.Models;
 namespace Portal.Controllers
 {
     public class HomeController : BaseController
     {
         public ActionResult Index()
         {
-            return View();
+            string sql = @" select distinct d.* from Domain d inner join isler i on(d.DomainID=i.islerRefDomainID)
+ where DomainID in
+ (
+ select top 10 DomainID from
+ (
+ select  top 100  islerRefDomainID as DomainID
+ ,row_number() over (partition by islerRefDomainID order by islerTarih desc) as seqnum
+ from isler
+ order by islerTarih desc 
+ )t
+ where t.seqnum=1
+ ) ";
+            var list = Db.Database.SqlQuery<Domain>(sql).ToList();
+            return View(list);
         }
 
         public ActionResult About()
