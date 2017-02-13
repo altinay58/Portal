fa169c7769040c12140cecc6b45d9f07bcfd6ddf
fatih <fatihgokce07@gmail.com>
2017-02-13 13:56:50 +0300

    -timezone sorunu cozuldu

--- Portal/Controllers/HomeController.cs
+++ Portal/Controllers/HomeController.cs
@@ -4,25 +4,41 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Portal.Models;
+using Microsoft.AspNet.Identity;
+
 namespace Portal.Controllers
 {
     public class HomeController : BaseController
     {
         public ActionResult Index()
         {
-            string sql = @" select distinct d.* from Domain d inner join isler i on(d.DomainID=i.islerRefDomainID)
- where DomainID in
+            string sql = @"select d.*
+ from
  (
- select top 10 DomainID from
- (
- select  top 100  islerRefDomainID as DomainID
- ,row_number() over (partition by islerRefDomainID order by islerTarih desc) as seqnum
- from isler
- order by islerTarih desc 
- )t
- where t.seqnum=1
- ) ";
+ select  i.islerRefDomainID as DomainID,i.islerTarih,
+ (select count(i2.islerRefDomainID) from isler i2 where i2.islerRefDomainID=i.islerRefDomainID) cnt,
+ row_number() over (partition by i.islerRefDomainID order by i.islerTarih desc) as seqnum
+ from isler i inner join Domain d on (i.islerRefDomainID=d.DomainID)
+
+ ) r,Domain d
+ where seqnum=1 and 
+ d.DomainID=r.DomainID
+  order by islerTarih desc";
             var list = Db.Database.SqlQuery<Domain>(sql).ToList();
+
+            string userid = User.Identity.GetUserId();
+
+            List<int> idler = Db.IsiYapacakKisis.Where(a => a.RefIsiYapacakKisiUserID == userid).Select(x => x.RefIsID).ToList();
+
+            var yapacagiisler = Db.islers.Where(a => idler.Contains(a.islerID) && a.islerisinTamamlanmaDurumu != true).OrderBy(a => a.islerTarih).ToList();
+
+            var verdigiisler = Db.islers.Where(a => a.islerisiVerenKisi == userid && a.islerisinTamamlanmaDurumu != true).OrderBy(a => a.islerSira).ToList();
+
+            //IEnumerable<isler> yeniIslerim = Partial.KullaniciBildirim(userid).yeniIsler;
+            //IEnumerable<isler> kontrolBekleyenIsler = Partial.KullaniciBildirim(userid).kontrolBekleyenIsler;
+            //IEnumerable<isler> okunmayanIsler = Partial.CevaplananIslerOkunmayanlar(userid);
+            //IEnumerable<Randevu> randevular = Partial.KullaniciBildirim(iduserid.Randevular;
+            //int bildirimSayisi = Partial.KullaniciBildirim(userid).BildirimSayisi;
             return View(list);
         }
 
