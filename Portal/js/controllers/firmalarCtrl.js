df739f83808e99cb8b4518a23cfda2727b35147b
fatih <fatihgokce07@gmail.com>
2017-03-29 23:04:09 +0300

    *iş planı
    -aynı gün de aynı iş birden fazla eklenmemis için kontrol eklendi
    -firmalar tab i eklendi

--- Portal/js/controllers/firmalarCtrl.js
+++ Portal/js/controllers/firmalarCtrl.js
@@ -1,24 +1,23 @@
-﻿var bfc;
-angModule.controller("borcluFirmalarCtrl", function ($scope, $timeout) {
+﻿var fc;
+angModule.controller("firmalarCtrl", function ($scope, $timeout) {
+
     let self = $scope;
-    self.yukleniyor = false;
     $scope.maxSize = 5;
-  
     angular.element(document).ready(function () {
-        bfc = self;
+        fc = self;
         self.getirData();
         console.log(self.page);
     });
     self.init = function (bolgelerJsn) {
         self.bolgeler = JSON.parse(bolgelerJsn);
     };
-    self.getirData = function (model,bolgemi) {
+    self.getirData = function (model, bolgemi) {
         self.yukleniyor = true;
         if ((model === undefined || model === "") || (model.length >= 3 || bolgemi)) {
-            commonAjaxService.getDataFromRemote(url = "/IsPlani/BorcluFirmalarAra", data = {
+            commonAjaxService.getDataFromRemote(url = "/IsPlani/FirmaAra", data = {
                 konumId: self.seciliBolge, page: self.page, firmaAdi: self.firmaAdi, telNo: self.telNo, cepTelNo: self.cepTelNo,
                 yetkili: self.yetkiliAdi
-                
+
             }).done(res=> {
                 self.firmalar = res.Data;
                 $scope.totalItems = res.ToplamSayi;
@@ -35,6 +34,6 @@ angModule.controller("borcluFirmalarCtrl", function ($scope, $timeout) {
         console.log(f);
         $("#modalIsPlani").modal("show");
         let isPlaniScope = angular.element($("[ng-controller='isPlaniCtrl']")).scope();
-        isPlaniScope.initData(etiketIsPlaniTipi=isPlaniScope.etiketIsPlaniTipleri.Borclu,refId=f.firma.FirmaID);
+        isPlaniScope.initData(etiketIsPlaniTipi = isPlaniScope.etiketIsPlaniTipleri.Firma, refId = f.firma.FirmaID);
     };
 })
