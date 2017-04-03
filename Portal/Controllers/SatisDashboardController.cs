c01f66a445b20142e91c94011bf46c5163dae380
fatih <fatihgokce07@gmail.com>
2017-04-03 23:38:10 +0300

    -satis dashboard tasarimi yapılıyor

---
+++ Portal/Controllers/SatisDashboardController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portal.Controllers
+{
+    public class SatisDashboardController : BaseController
+    {
+        public SatisDashboardController()
+        {
+            GuncelMenu = "Satis Bolumu";
+        }
+        // GET: SatisDashboard
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
